601ebd071595f6ce05e5e5c5cf20707f53e470b0
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-03-11 21:59:56 -0700

    file1.md

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+const messageNames = [];
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push([`Thank you, ${names[i]}, for the wonderful ${value} gift!`]);
+    return messageNames; }
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise")
