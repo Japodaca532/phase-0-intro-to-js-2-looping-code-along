e7f07d8c9ac311ab41d78c70fd0b0687aa3d5b8e
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-04-29 17:15:06 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const names = ["Guadalupe", "Ollie", "Aki"];
 function writeCards(names, value) {
     let messageNames = [];
     for (let i = 3; i < names.length; i++) {
-        messageNames.push = (`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+        messageNames.push = ([`Thank you, ${names[i]}, for the wonderful ${value} gift!`]);
     }
     return messageNames;
 }
